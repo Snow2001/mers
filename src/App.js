0f1948eb9496362bbf1e56cb3aae386f39968b2a
master <tohirovalisher18@gmail.com>
2022-03-01 11:37:09 +0500

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './style/style.css'
 
 function App() {
+  const [count, setCount]= useState(0)
+  const [toggleBtn, setToggleBtn] = useState(false)
+  const [value, setValue] = useState("Alisher")
+
+  function inc(){
+    setCount(count + 1)
+  }
+  function dec(){
+    if(count > 0){
+      setCount(count - 1)
+    }
+    else{
+      setCount(count)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <p className="fs-3 m-3">Count: {count}</p>
+        <button 
+        onClick={inc}
+        className="btn btn-success m-3"
+        >INCR</button>
+        <button 
+        onClick={dec}
+        className="btn btn-danger">DECR</button>
+      </div>
+      <div className="app">
+        <button className="btn btn-success m-3"
+        onClick={()=> setToggleBtn(!toggleBtn)}>
+          Toggle
+        </button>
+        {toggleBtn ? <h3 className="m-3">You tube</h3> : null}
+        <hr/>
+        <p>Value: {value}</p>
+        <input type="text" className="form-control" value={value}  onChange={event => setValue(event.target.value)} />
+      </div>
+    </>
   );
 }
 
